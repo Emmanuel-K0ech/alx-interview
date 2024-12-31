39b772f10de70b0926b2996b28e581f0db7e678e
Emmanuel-K0ech <kipchumbakoech1107@gmail.com>
2024-12-30 22:11:14 -0500

    Code refactor

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,38 +1,25 @@
 #!/usr/bin/node
-// Star Wars Characters of a particular Film
-const request = require('request');
+// Star Wars Characters of a particulars Film using Async/Await
+const axios = require('axios');
 
 if (process.argv.length !== 3) {
-  console.log('Usage: ./0-starwars_characters.js <movie ID');
+  console.error('Usage: ./0-starwars_characters.js <movie ID>');
   process.exit(1);
 }
 
 const movieId = process.argv[2];
 const baseURL = 'https://swapi-api.alx-tools.com/api/films/';
-const url = `${baseURL}${movieId}`;
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-
+(async () => {
   try {
-    const data = JSON.parse(body);
+    const { data } = await axios.get(`${baseURL}${movieId}`);
     const characters = data.characters;
 
-    characters.forEach((characterUrl) => {
-      request(characterUrl, (error, response, body) => {
-        if (error) {
-          console.error(error);
-          return;
-        }
-        const person = JSON.parse(body);
-        const name = person.name;
-        console.log(name);
-      });
-    });
+    for (const characterUrl of characters) {
+      const { data: characterData } = await axios.get(characterUrl);
+      console.log(characterData.name);
+    }
   } catch (error) {
-    console.error('Error parsing JSON', error);
+    console.error('Error fetching data:', error.message);
   }
-});
+})();
